4a87d8265ac6f0e22d4905595cced740c31c8e37
Putta Khunchalee <putta.khu@wedev.co.th>
2020-10-08 12:30:31 +0700

    Adds ServiceCollectionExtensions for RabbitMQ

---
+++ src/Polybus.RabbitMQ/ServiceCollectionExtensions.cs
@@ -0,0 +1,35 @@
+namespace Microsoft.Extensions.DependencyInjection
+{
+    using System;
+    using Microsoft.Extensions.DependencyInjection.Extensions;
+    using Polybus;
+    using Polybus.RabbitMQ;
+    using RabbitMQ.Client;
+
+    public static class ServiceCollectionExtensions
+    {
+        public static void ConfigureRabbitMQEventBus(this IServiceCollection services, Action<EventBusOptions> options)
+        {
+            services
+                .AddOptions<EventBusOptions>()
+                .Configure(options)
+                .ValidateDataAnnotations();
+        }
+
+        public static void AddRabbitMQConnection(this IServiceCollection services, IAsyncConnectionFactory factory)
+        {
+            if (!factory.DispatchConsumersAsync)
+            {
+                throw new ArgumentException("The factory required to enabled DispatchConsumersAsync.", nameof(factory));
+            }
+
+            // FIXME: Find a solution to close the connection when disposing.
+            services.TryAddSingleton<IConnection>(p => factory.CreateConnection());
+        }
+
+        public static void AddRabbitMQPublisher(this IServiceCollection services)
+        {
+            services.TryAddSingleton<IEventPublisher, EventPublisher>();
+        }
+    }
+}
